141a4de08706aafaac579de119f7cb657e8f4c49
Alexander Gould <alexander.gould94@gmail.com>
2022-01-26 17:55:16 -0800

    commit tree 563aafc14005cc7e4d7638617fea20c309d0d425

--- index.js
+++ index.js
@@ -9,6 +9,62 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord(arr){
+     return {
+         firstName: arr[0],
+         familyName: arr[1],
+         title: arr[2],
+         payPerHour: arr[3],
+         timeInEvents: [],
+         timeOutEvents: []
+     }
+ }
+
+ function createEmployeeRecords(arr){
+     const records = arr.map(a=>createEmployeeRecord(a))
+     return records
+ }
+
+ function createTimeInEvent(date){
+    const split = date.split(' ')
+    const d = split[0]
+    const t = parseInt(split[1])
+    const timeIn = {
+        type: "TimeIn",
+        date: d,
+        hour: t
+    }
+    this.timeInEvents.push(timeIn)
+    return this
+ }
+
+ function createTimeOutEvent(date){
+    const split = date.split(' ')
+    const d = split[0]
+    const t = parseInt(split[1])
+    const timeOut = {
+        type: "TimeOut",
+        date: d,
+        hour: t
+    }
+    this.timeOutEvents.push(timeOut)
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    const timeIn = this.timeInEvents.find(e => e.date === date);
+    const timeOut = this.timeOutEvents.find(e => e.date === date);
+    const hrs = (timeOut.hour - timeIn.hour)/100
+    return hrs
+}
+
+function wagesEarnedOnDate(date){
+    
+    const hrs = hoursWorkedOnDate.call(this, date)
+    const wages = this.payPerHour * hrs
+    return wages
+}
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +77,14 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstNameString){
+    const n = srcArray.find(e => e.firstName = firstNameString)
+    return n
+}
+
+function calculatePayroll(employees){
+    const allWages = employees.map(e => allWagesFor.call(e))
+    const reducer = (a, b) => a+b;
+    const payRoll = allWages.reduce(reducer, 0)
+    return payRoll
+}
